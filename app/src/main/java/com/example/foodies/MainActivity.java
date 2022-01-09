ecb672dd43880b6e33dcbd3d3dea5212773e0897
ShenhavDaf <shenhav.dafadi@gmail.com>
2022-01-09 17:09:39 +0200

    Login Page design

--- app/src/main/java/com/example/foodies/MainActivity.java
+++ app/src/main/java/com/example/foodies/MainActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginBtn;
+    TextView joinTv;
     Intent joinIntent, homePageIntent;
 
     @Override
@@ -18,13 +21,14 @@ public class MainActivity extends AppCompatActivity {
         joinIntent = new Intent(this, SignIn.class);
         homePageIntent = new Intent(this, HomePage.class);
 
-        Button joinBtn = findViewById(R.id.main_join_btn);
-        joinBtn.setOnClickListener(new View.OnClickListener() {
+        joinTv = findViewById(R.id.main_joinbtn_tv);
+        joinTv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { JoinUs();  }
         });
 
-        Button loginBtn = findViewById(R.id.main_login_btn);
+
+        loginBtn = findViewById(R.id.main_login_btn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { LogIn();  }
