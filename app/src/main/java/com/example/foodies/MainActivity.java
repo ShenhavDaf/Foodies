d67f3fc64bed0ab0667a558d026217b35aef480e
LilachBachar <Lilachbac@gmail.com>
2022-01-11 17:12:25 +0200

    Fix back button (at the top menu).

--- app/src/main/java/com/example/foodies/MainActivity.java
+++ app/src/main/java/com/example/foodies/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.foodies;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.NavHost;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.content.Intent;
@@ -13,17 +15,27 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AppBarConfiguration mAppBarConfiguration;
     NavController navCtl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAppBarConfiguration = new AppBarConfiguration.Builder().build();
 
         NavHost navHost = (NavHost) getSupportFragmentManager().findFragmentById(R.id.main_navhost);
         navCtl = navHost.getNavController();
 
         NavigationUI.setupActionBarWithNavController(this,navCtl);
+
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.main_navhost);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)|| super.onSupportNavigateUp();
     }
 
 }
