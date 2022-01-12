9462fb17ce1bfec53cef3ee654d4547dafe18eb6
LilachBachar <Lilachbac@gmail.com>
2022-01-12 13:26:22 +0200

    Add action from profile to editProfile in nav_graph and from signin to homepage. Add first design of Profile. Add setError in SignIn. Change labels name.

--- app/src/main/java/com/example/foodies/ProfileFragment.java
+++ app/src/main/java/com/example/foodies/ProfileFragment.java
@@ -2,13 +2,24 @@ package com.example.foodies;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
+
+    Button editProfileBtn;
+    TextView fullNameTv;
+//    MyAdapter adapter;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,6 +27,43 @@ public class ProfileFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
         // Inflate the layout for this fragment
+
+        fullNameTv = view.findViewById(R.id.profile_fullname_tv);
+
+        editProfileBtn = view.findViewById(R.id.profile_editprofile_btn);
+        editProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editProfile(v);
+            }
+        });
+
+        RecyclerView list = view.findViewById(R.id.profile_postslist_rv);
+        list.setHasFixedSize(true);
+        list.setLayoutManager(new LinearLayoutManager(getContext()));
+
+
+//
+//        list.setAdapter(adapter);
+//
+//        adapter.setOnItemClickListener(new HomePageFragment.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(View v,int position) {
+////                String stId = viewModel.getData().getValue().get(position).getId();
+////                Navigation.findNavController(v).navigate(StudentListRvFragmentDirections.actionStudentListRvFragmentToStudentDetailsFragment(stId));
+//
+//                System.out.println("to the post page");
+//            }
+//        });
+
+
+
         return view;
+    }
+
+
+    private void editProfile(View view) {
+        System.out.println("edit profile was clicked");
+        Navigation.findNavController(view).navigate(R.id.action_profileFragment_to_editProfileFragment);
     }
 }
--- app/src/main/java/com/example/foodies/SignInFragment.java
+++ app/src/main/java/com/example/foodies/SignInFragment.java
@@ -2,13 +2,24 @@ package com.example.foodies;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
 
 public class SignInFragment extends Fragment {
+
+    Button join;
+    EditText fullNameEt, emailEt, passwordEt, verifyEt,  cityEt, birthdayEt;
+    // TODO: add image, add verify
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -16,6 +27,70 @@ public class SignInFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
         // Inflate the layout for this fragment
+        fullNameEt = view.findViewById(R.id.signin_name_et);
+        emailEt = view.findViewById(R.id.signin_email_et);
+        passwordEt = view.findViewById(R.id.signin_password_et);
+        cityEt = view.findViewById(R.id.signin_city_et);
+        birthdayEt = view.findViewById(R.id.signin_birthdate_et);
+
+        join = view.findViewById(R.id.signin_join_btn);
+        join.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Join(view);
+            }
+        });
         return view;
+    }
+
+    private void Join(View view){
+        System.out.println("join button was clicked");
+
+        String fullname = fullNameEt.getText().toString();
+        String email = emailEt.getText().toString();
+        String password = passwordEt.getText().toString();
+//        String verify = verifyEt.getText().toString();
+        String city = cityEt.getText().toString();
+        String birthday = birthdayEt.getText().toString();
+
+        if(fullname.isEmpty()){
+            fullNameEt.setError("Please enter your full name");
+            fullNameEt.requestFocus();
+            return;
+        }
+        if(email.isEmpty()){
+            emailEt.setError("Please enter your Email");
+            emailEt.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEt.setError("Please provide valid email");
+            emailEt.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            passwordEt.setError("Please enter your password");
+            passwordEt.requestFocus();
+            return;
+        }
+        if(password.length() < 5){
+            passwordEt.setError("Password length should be at least 5 characters");
+            passwordEt.requestFocus();
+            return;
+
+        }
+        if(city.isEmpty()){
+            cityEt.setError("Please enter your city");
+            cityEt.requestFocus();
+            return;
+        }
+        if(birthday.isEmpty()){
+            birthdayEt.setError("Please enter your birthday");
+            birthdayEt.requestFocus();
+            return;
+        }
+
+
+        Navigation.findNavController(view).navigate(R.id.action_signIn_to_homePage);
     }
 }
